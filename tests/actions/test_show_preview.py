64ffe6a1b83f9a8407178678ae712b11a943a8aa
Fabricio Floriano <f.florian0@hotmail.com>
2023-11-18 23:12:20 -0300

    Requisito 03? - Test func show_preview

--- tests/actions/test_show_preview.py
+++ tests/actions/test_show_preview.py
@@ -1,1 +1,34 @@
 from pro_filer.actions.main_actions import show_preview  # NOQA
+import pytest
+
+
+@pytest.mark.parametrize(
+    "context, expected",
+    [
+        (
+            {
+                "all_files": [
+                    "file1",
+                    "file2",
+                    "file3",
+                    "file4",
+                    "file5",
+                    "file6",
+                ],
+                "all_dirs": ["src", "src/utils"],
+            },
+            "Found 6 files and 2 directories\n"
+            "First 5 files: ['file1', 'file2', 'file3', 'file4', 'file5']\n"
+            "First 5 directories: ['src', 'src/utils']\n",
+        ),
+        (
+            {"all_files": [], "all_dirs": []},
+            "Found 0 files and 0 directories\n",
+        ),
+    ],
+)
+def test_show_preview(capsys, context, expected):
+    show_preview(context)
+    captured = capsys.readouterr()
+
+    assert captured.out == expected
